e0e4d0670ea51caddf7fa179980ea8fc69845f6d
tommac1204 <tom@graphitti.org>
2014-12-10 16:15:27 +0000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,31 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+import requests
+import scraperwiki.sqlite as db
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+PARENT_ID = 2513# London Borough of Haringey
+
+
+
+def iter_children_areas_kml(parent_id):
+    children = getjs('http://mapit.mysociety.org/area/%s/children' % parent_id)
+    for id, data in children.items():
+        kml = requests.get('http://mapit.mysociety.org/area/%s.kml' % id).content
+        entry = {'parent_area': int(data['parent_area']),
+                 'id': int(id),
+                 'name': data['name'],
+                 'kml': kml}
+        yield entry
+
+
+def getjs(url, **opts):
+    return json.loads(requests.get(url, **opts).content)
+
+
+#
+# Main
+#
+data = list(iter_children_areas_kml(PARENT_ID))
+db.save(['id'], data, verbose=0)
